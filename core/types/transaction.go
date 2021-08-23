8de5efa1610ac642d245d9de91d143ce39815edb
Marius van der Wijden <m.vanderwijden@live.de>
2021-08-23 11:49:39 +0200

    core: only check sendernoeoa in non fake mode (#23424)

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -581,11 +581,11 @@ type Message struct {
 	gasTipCap    *big.Int
 	data         []byte
 	accessList   AccessList
-	checkNonce   bool
+	isFake       bool
 	fromExternal bool
 }
 
-func NewMessage(from common.Address, to *common.Address, nonce uint64, amount *big.Int, gasLimit uint64, gasPrice, gasFeeCap, gasTipCap *big.Int, data []byte, accessList AccessList, checkNonce bool) Message {
+func NewMessage(from common.Address, to *common.Address, nonce uint64, amount *big.Int, gasLimit uint64, gasPrice, gasFeeCap, gasTipCap *big.Int, data []byte, accessList AccessList, isFake bool) Message {
 	return Message{
 		from:         from,
 		to:           to,
@@ -597,7 +597,7 @@ func NewMessage(from common.Address, to *common.Address, nonce uint64, amount *b
 		gasTipCap:    gasTipCap,
 		data:         data,
 		accessList:   accessList,
-		checkNonce:   checkNonce,
+		isFake:       isFake,
 		fromExternal: false,
 	}
 }
@@ -614,7 +614,7 @@ func (tx *Transaction) AsMessage(s Signer, baseFee *big.Int) (Message, error) {
 		amount:     tx.Value(),
 		data:       tx.Data(),
 		accessList: tx.AccessList(),
-		checkNonce: true,
+		isFake:     false,
 	}
 	// If baseFee provided, set gasPrice to effectiveGasPrice.
 	if baseFee != nil {
@@ -641,5 +641,5 @@ func (m Message) Gas() uint64            { return m.gasLimit }
 func (m Message) Nonce() uint64          { return m.nonce }
 func (m Message) Data() []byte           { return m.data }
 func (m Message) AccessList() AccessList { return m.accessList }
-func (m Message) CheckNonce() bool       { return m.checkNonce }
+func (m Message) IsFake() bool           { return m.isFake }
 func (m Message) FromExternal() bool     { return m.fromExternal }
