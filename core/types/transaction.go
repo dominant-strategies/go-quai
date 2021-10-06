54ff2eed215b7228800ae0b53f402c5811e645df
aaronbuchwald <aaron.buchwald56@gmail.com>
2021-10-06 07:12:52 -0400

    core/types: copy tx recipient address (#23376)
    This resolves a long-standing TODO. The point of copying the address is
    to ensure that all data referenced by types.Transaction is independent of the
    data passed into the constructor.
    
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -286,13 +286,7 @@ func (tx *Transaction) Nonce() uint64 { return tx.inner.nonce() }
 // To returns the recipient address of the transaction.
 // For contract-creation transactions, To returns nil.
 func (tx *Transaction) To() *common.Address {
-	// Copy the pointed-to address.
-	ito := tx.inner.to()
-	if ito == nil {
-		return nil
-	}
-	cpy := *ito
-	return &cpy
+	return copyAddressPtr(tx.inner.to())
 }
 
 // Cost returns gas * gasPrice + value.
@@ -643,3 +637,12 @@ func (m Message) Data() []byte           { return m.data }
 func (m Message) AccessList() AccessList { return m.accessList }
 func (m Message) IsFake() bool           { return m.isFake }
 func (m Message) FromExternal() bool     { return m.fromExternal }
+
+// copyAddressPtr copies an address.
+func copyAddressPtr(a *common.Address) *common.Address {
+	if a == nil {
+		return nil
+	}
+	cpy := *a
+	return &cpy
+}
