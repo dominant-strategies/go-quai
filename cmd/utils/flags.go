3bb65e25bf47ae33a9d3029730eb9a7281174078
Alan Orwick <alan.kev11@gmail.com>
2021-11-15 12:29:15 -0600

    Added mainnet bootnodes

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -33,6 +33,8 @@ import (
 	"text/template"
 	"time"
 
+	pcsclite "github.com/gballet/go-libpcsclite"
+	gopsutil "github.com/shirou/gopsutil/mem"
 	"github.com/spruce-solutions/go-quai/accounts"
 	"github.com/spruce-solutions/go-quai/accounts/keystore"
 	"github.com/spruce-solutions/go-quai/common"
@@ -67,8 +69,6 @@ import (
 	"github.com/spruce-solutions/go-quai/p2p/nat"
 	"github.com/spruce-solutions/go-quai/p2p/netutil"
 	"github.com/spruce-solutions/go-quai/params"
-	pcsclite "github.com/gballet/go-libpcsclite"
-	gopsutil "github.com/shirou/gopsutil/mem"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -861,7 +861,7 @@ func setBootstrapNodes(ctx *cli.Context, cfg *p2p.Config) {
 	cfg.BootstrapNodes = make([]*enode.Node, 0, len(urls))
 	for _, url := range urls {
 		if url != "" {
-			node, err := enode.Parse(enode.ValidSchemes, url)
+			node, err := enode.Parse(enode.ValidSchemes, url+cfg.ListenAddr)
 			if err != nil {
 				log.Crit("Bootstrap URL invalid", "enode", url, "err", err)
 				continue
