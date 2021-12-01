70e5574b40bd5c5642ab137e60ea6e297178b107
Alan Orwick <alan.kev11@gmail.com>
2021-12-01 14:09:23 -0600

    Reworked bloom to still add even if nil

--- core/bloom_indexer.go
+++ core/bloom_indexer.go
@@ -67,9 +67,11 @@ func (b *BloomIndexer) Reset(ctx context.Context, section uint64, lastSectionHea
 // Process implements core.ChainIndexerBackend, adding a new header's bloom into
 // the index.
 func (b *BloomIndexer) Process(ctx context.Context, header *types.Header) error {
+	bloom := types.Bloom{}
 	if len(header.Bloom) > 0 {
-		b.gen.AddBloom(uint(header.Number[types.QuaiNetworkContext].Uint64()-b.section*b.size), header.Bloom[types.QuaiNetworkContext])
+		bloom = header.Bloom[types.QuaiNetworkContext]
 	}
+	b.gen.AddBloom(uint(header.Number[types.QuaiNetworkContext].Uint64()-b.section*b.size), bloom)
 	b.head = header.Hash()
 	return nil
 }
