88de779293bda0cc93d75388369e269bf02cf42c
Alan Orwick <alan.kev11@gmail.com>
2022-06-06 17:40:44 -0500

    New bootnode URLs (#272)
    * New Ropsten bootnode in GCP
    
    * Moved bootnode out of Ropsten to Mainnet

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -21,9 +21,7 @@ import "github.com/spruce-solutions/go-quai/common"
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main Quai Network.
 var MainnetBootnodes = []string{
-	"enode://a14dc9ceaebdcfb1149ba1dd489e8743f268b517c511c215976e2e71cb39e778b6b9e767ae05e9574cdbd10733458ebf94e06ce18524f4c4b7673e378b0bddd0@140.82.19.149", // vultr-full-node-1
-	"enode://794367029090f82f56b079aa55b4f95f1767ff8de78482ed2a2604d970065017e1da77ac8492a1588abf9673635414f7383068328e8428d1e46902a140eddf14@45.76.19.78",   // vultr-full-node-2
-	"enode://9f8acf7988b21ee7bb5772502173e7bc902843190ce31857d13052474dd167ce479577f536cda07dafc84d63fbaad36016274aca74d2c69215f7cc081ef05e04@45.32.77.104",  // vultr-full-node-3
+	"enode://6e79bc43bb98a582805a7bf81cfd67cd193c68bebcc9609d939df7576374d5ec3c71a2b70e59993c0a3cd8c5bb5dfae74c3efae72c8e7bd8d2370a84da33baf7@34.135.197.187", // ds-team-node
 }
 
 // RopstenBootnodes are the enode URLs of the P2P bootstrap nodes running on the
