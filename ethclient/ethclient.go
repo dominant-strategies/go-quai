3509982dcfac26b5841a31b18edacf75149f6193
Alan Orwick <alan.kev11@gmail.com>
2022-06-16 16:25:12 -0500

    Ethclient updates to receipts

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -374,14 +374,7 @@ func (ec *Client) TransactionReceipt(ctx context.Context, txHash common.Hash) (*
 
 // GetBlockReceipts returns the receipts of a block by block hash.
 func (ec *Client) GetBlockReceipts(ctx context.Context, blockHash common.Hash) (*types.ReceiptBlock, error) {
-	var r *types.ReceiptBlock
-	err := ec.c.CallContext(ctx, &r, "quai_getBlockWithReceiptsByHash", blockHash)
-	if err == nil {
-		if r == nil {
-			return nil, ethereum.NotFound
-		}
-	}
-	return r, err
+	return ec.getBlockWithReceipts(ctx, "quai_getBlockWithReceiptsByHash", blockHash)
 }
 
 type rpcProgress struct {
