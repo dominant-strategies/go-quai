083b0dc305e5e29c0ea500ffaa58c92ed3b90a31
Alan Orwick <alan.kev11@gmail.com>
2022-06-30 14:39:33 -0500

    handler.go: updated AddExternalBlock call

--- eth/handler.go
+++ eth/handler.go
@@ -220,7 +220,7 @@ func newHandler(config *handlerConfig) (*handler, error) {
 		}
 		return n, err
 	}
-	h.blockFetcher = fetcher.NewBlockFetcher(false, nil, h.chain.GetBlockByHash, validator, h.BroadcastBlock, heighter, nil, inserter, h.removePeer, h.chain.GetLinkExternalBlocks)
+	h.blockFetcher = fetcher.NewBlockFetcher(false, nil, h.chain.GetBlockByHash, validator, h.BroadcastBlock, heighter, nil, inserter, h.removePeer, h.chain.GetLinkExternalBlocks, h.chain.AddExternalBlocks)
 
 	fetchTx := func(peer string, hashes []common.Hash) error {
 		p := h.peers.peer(peer)
