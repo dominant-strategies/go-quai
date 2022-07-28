2e02ab5fe3ca3df416d758d115f7328ab222f9cc
kiltsonfire <106615399+kiltsonfire@users.noreply.github.com>
2022-07-28 14:35:29 -0500

    reverting changes to Kademlia so we can keep track (#390)
    Co-authored-by: Joe <joe@dominantstrategies.io>

--- p2p/discover/table.go
+++ p2p/discover/table.go
@@ -39,7 +39,7 @@ import (
 )
 
 const (
-	alpha           = 6  // Kademlia concurrency factor
+	alpha           = 3  // Kademlia concurrency factor
 	bucketSize      = 16 // Kademlia bucket size
 	maxReplacements = 10 // Size of per-bucket replacement list
 
@@ -50,13 +50,13 @@ const (
 	bucketMinDistance = hashBits - nBuckets // Log distance of closest bucket
 
 	// IP address limits.
-	bucketIPLimit, bucketSubnet = 24, 24 // at most 2 addresses from the same /24
-	tableIPLimit, tableSubnet   = 24, 24
+	bucketIPLimit, bucketSubnet = 2, 24 // at most 2 addresses from the same /24
+	tableIPLimit, tableSubnet   = 10, 24
 
 	refreshInterval    = 30 * time.Minute
 	revalidateInterval = 10 * time.Second
 	copyNodesInterval  = 30 * time.Second
-	seedMinTableTime   = 1 * time.Second
+	seedMinTableTime   = 5 * time.Minute
 	seedCount          = 30
 	seedMaxAge         = 5 * 24 * time.Hour
 )
