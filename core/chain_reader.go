bf588ac482289b36f0d45f18862891b687049ea4
shreekara shastry <66725577+shreekarashastry@users.noreply.github.com>
2022-07-30 16:01:14 -0500

    chain_Reader.go: Created a New placeholder for chainreader from forker for reuse

---
+++ core/chain_reader.go
@@ -0,0 +1,42 @@
+package core
+
+import (
+	"math/big"
+
+	"github.com/spruce-solutions/go-quai/common"
+	"github.com/spruce-solutions/go-quai/core/types"
+	"github.com/spruce-solutions/go-quai/params"
+)
+
+// ChainReader defines a small collection of methods needed to access the local
+// blockchain during header verification. It's implemented by both blockchain
+// and lightchain.
+type ChainReader interface {
+	// Config retrieves the header chain's chain configuration.
+	Config() *params.ChainConfig
+
+	// GetTd returns the total difficulty of a local block.
+	GetTd(common.Hash, uint64) []*big.Int
+
+	// CalcTd calculates the TD of the given header using PCRC and CalcHLCRNetDifficulty.
+	CalcTd(header *types.Header) ([]*big.Int, error)
+
+	// GetBlockByHash retrieves a block from the database by hash, caching it if found.
+	GetBlockByHash(hash common.Hash) *types.Block
+
+	// GetHeaderByHash retrieves a block header from the database by hash, caching it if
+	// found.
+	GetHeaderByHash(hash common.Hash) *types.Header
+
+	// HLCR does hierarchical comparison of two difficulty tuples and returns true if second tuple is greater than the first
+	HLCR(localDifficulties []*big.Int, externDifficulties []*big.Int) bool
+
+	// DomReorgNeeded checks the dominant chain for the reorg status.
+	DomReorgNeeded(header *types.Header) (bool, error)
+
+	// PCCRC The purpose of the Previous Coincident Reference Check (PCRC) is to establish
+	PCCRC(header *types.Header, headerOrder int) (types.PCRCTermini, error)
+
+	// Gets the difficulty order of a header
+	GetDifficultyOrder(header *types.Header) (int, error)
+}
