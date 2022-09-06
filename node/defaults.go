d68fe81c6989e63c5d694bf311d95efc4b30b08c
wizeguyy <wizeguyy@dominantstrategies.io>
2022-09-06 13:49:34 -0500

    Set global node location at startup

--- node/defaults.go
+++ node/defaults.go
@@ -61,19 +61,19 @@ func DefaultDataDir() string {
 	if home != "" {
 		switch runtime.GOOS {
 		case "darwin":
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "Quai")
 		case "windows":
 			// We used to put everything in %HOME%\AppData\Roaming, but this caused
 			// problems with non-typical setups. If this fallback location exists and
 			// is non-empty, use it, otherwise DTRT and check %LOCALAPPDATA%.
-			fallback := filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			fallback := filepath.Join(home, "AppData", "Roaming", "Quai")
 			appdata := windowsAppData()
 			if appdata == "" || isNonEmptyDir(fallback) {
 				return fallback
 			}
-			return filepath.Join(appdata, "Ethereum")
+			return filepath.Join(appdata, "Quai")
 		default:
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".quai")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
