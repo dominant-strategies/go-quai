bb50af97891030d647e186c7a1fdf6d58ed75745
wizeguyy <wizeguyy@dominantstrategies.io>
2022-09-21 13:23:48 -0500

    Remove support for legacy transaction types & consolidate signers
    This patch consolidates all the various Ethereum transaction types into one
    transaction type equivalent to the latest London TX type (i.e. dynamic fee
    type). Related logic is also updated accordingly (e.g. basefee is no longer
    optional).

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -570,11 +570,7 @@ func (pool *TxPool) local() map[common.Address]types.Transactions {
 // rules and adheres to some heuristic limits of the local node (price and size).
 func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 	// Accept only legacy transactions until EIP-2718/2930 activates.
-	if !pool.eip2718 && tx.Type() != types.LegacyTxType {
-		return ErrTxTypeNotSupported
-	}
-	// Reject dynamic fee transactions until EIP-1559 activates.
-	if !pool.eip1559 && tx.Type() == types.DynamicFeeTxType {
+	if !pool.eip2718 {
 		return ErrTxTypeNotSupported
 	}
 	// Reject transactions over defined size to prevent DOS attacks
