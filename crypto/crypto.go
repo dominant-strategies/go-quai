156a61a3023f53e7fa9f1bc50e5cd6dd5787a5e5
Jonathan Downing <jonathan@dominantstrategies.io>
2022-10-07 15:31:18 -0500

    Modified CreateAddress to include tx data for contract address creation

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -105,8 +105,8 @@ func Keccak512(data ...[]byte) []byte {
 }
 
 // CreateAddress creates an ethereum address given the bytes and the nonce
-func CreateAddress(b common.Address, nonce uint64) common.Address {
-	data, _ := rlp.EncodeToBytes([]interface{}{b, nonce})
+func CreateAddress(b common.Address, nonce uint64, code []byte) common.Address {
+	data, _ := rlp.EncodeToBytes([]interface{}{b, nonce, code})
 	return common.BytesToAddress(Keccak256(data)[12:])
 }
 
