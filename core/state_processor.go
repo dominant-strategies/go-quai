25e36237438c8a2d7f03fdf29fe38aa95a8228af
wizeguyy <wizeguyy@dominantstrategies.io>
2022-10-20 16:39:39 -0500

    Record emitted ETXs in receipts

--- core/state_processor.go
+++ core/state_processor.go
@@ -250,7 +250,7 @@ func applyTransaction(msg types.Message, config *params.ChainConfig, bc ChainCon
 
 	// Create a new receipt for the transaction, storing the intermediate root and gas used
 	// by the tx.
-	receipt := &types.Receipt{Type: tx.Type(), PostState: root, CumulativeGasUsed: *usedGas}
+	receipt := &types.Receipt{Type: tx.Type(), PostState: root, CumulativeGasUsed: *usedGas, Etxs: result.Etxs}
 	if result.Failed() {
 		receipt.Status = types.ReceiptStatusFailed
 	} else {
