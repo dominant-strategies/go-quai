c6c7b7381ed99cc13627232e2d0959a1917c0a86
wizeguyy <wizeguyy@dominantstrategies.io>
2022-10-24 22:30:37 -0500

    Add GetSubManifest() quaiclient API

--- core/slice.go
+++ core/slice.go
@@ -374,6 +374,14 @@ func (sl *Slice) GetPendingHeader() (*types.Header, error) {
 	return sl.phCache[sl.pendingHeader].Header, nil
 }
 
+func (sl *Slice) GetSubManifest(blockHash common.Hash) (types.BlockManifest, error) {
+	header := sl.hc.GetHeaderByHash(blockHash)
+	if header == nil {
+		return nil, errors.New("block not found")
+	}
+	return sl.hc.CollectBlockManifest(header)
+}
+
 // SubRelayPendingHeader takes a pending header from the sender (ie dominant), updates the phCache with a composited header and relays result to subordinates
 func (sl *Slice) SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg bool) error {
 	nodeCtx := common.NodeLocation.Context()
