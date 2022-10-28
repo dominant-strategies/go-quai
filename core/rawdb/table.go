ee35900d14dcfe29ab941ff2ffcd2b36b5aa9558
wizeguyy <wizeguyy@dominantstrategies.io>
2022-10-28 12:55:26 -0500

    Add EtxSet type & db accessors

--- core/rawdb/table.go
+++ core/rawdb/table.go
@@ -76,8 +76,8 @@ func (t *table) AncientSize(kind string) (uint64, error) {
 
 // AppendAncient is a noop passthrough that just forwards the request to the underlying
 // database.
-func (t *table) AppendAncient(number uint64, hash, header, body, receipts, td []byte) error {
-	return t.db.AppendAncient(number, hash, header, body, receipts, td)
+func (t *table) AppendAncient(number uint64, hash, header, body, receipts, td, etxSet []byte) error {
+	return t.db.AppendAncient(number, hash, header, body, receipts, td, etxSet)
 }
 
 // TruncateAncients is a noop passthrough that just forwards the request to the underlying
