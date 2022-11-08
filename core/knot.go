eaaad4397e1b3bb0bf0437cf470bada6990a5465
wizeguyy <wizeguyy@dominantstrategies.io>
2022-11-08 16:17:45 -0600

    Reconstruct knot blocks with correct manifest for local context
    Since every block in the knot is a prime block, the manifest is always just the
    prior block in that context.

--- core/knot.go
+++ core/knot.go
@@ -6,11 +6,14 @@ import (
 	"os"
 	"strings"
 
+	"github.com/dominant-strategies/go-quai/common"
 	"github.com/dominant-strategies/go-quai/core/types"
 	"github.com/dominant-strategies/go-quai/rlp"
+	"github.com/dominant-strategies/go-quai/trie"
 )
 
 func ReadKnot(chainfile string) []*types.Block {
+	nodeCtx := common.NodeLocation.Context()
 	// Load chain.rlp.
 	fh, err := os.Open(chainfile)
 	if err != nil {
@@ -26,13 +29,20 @@ func ReadKnot(chainfile string) []*types.Block {
 	stream := rlp.NewStream(reader, 0)
 	var blocks = make([]*types.Block, 0)
 	for i := 0; ; i++ {
-		var b types.Block
-		if err := stream.Decode(&b); err == io.EOF {
+		b := &types.Block{}
+		if err := stream.Decode(b); err == io.EOF {
 			break
 		} else if err != nil {
 			return nil
 		}
-		blocks = append(blocks, &b)
+		h := b.Header()
+		// If we have a subordinate, we need to rebuild the block with the correct
+		// manifest of subordinate blocks
+		if nodeCtx < common.ZONE_CTX {
+			subManifest := types.BlockManifest{h.ParentHash(nodeCtx + 1)}
+			b = types.NewBlock(h, nil, nil, nil, subManifest, nil, trie.NewStackTrie(nil))
+		}
+		blocks = append(blocks, b)
 	}
 	return blocks
 }
