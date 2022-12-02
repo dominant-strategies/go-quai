a09230f893b634739c5b785cf6973b7ed5c18980
wizeguyy <wizeguyy@dominantstrategies.io>
2022-12-02 15:44:09 -0600

    Increase genesis gas limit & update genesis hashes

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -19,9 +19,9 @@ package params
 import "math/big"
 
 const (
-	GasLimitBoundDivisor uint64 = 1024    // The bound divisor of the gas limit, used in update calculations.
-	MinGasLimit          uint64 = 100000  // Minimum the gas limit may ever be.
-	GenesisGasLimit      uint64 = 4712388 // Gas limit of the Genesis block.
+	GasLimitBoundDivisor uint64 = 1024      // The bound divisor of the gas limit, used in update calculations.
+	MinGasLimit          uint64 = 10000000  // Minimum the gas limit may ever be.
+	GenesisGasLimit      uint64 = 471238800 // Gas limit of the Genesis block.
 
 	MaximumExtraDataSize  uint64 = 32    // Maximum size extra data may be after Genesis.
 	ExpByteGas            uint64 = 10    // Times ceil(log256(exponent)) for the EXP instruction.
