c1653688f21aaf9793f8db6c03ee891bb0623248
gop <gameofpointers@proton.me>
2022-12-06 13:07:51 -0600

    bugfix: CanonicalDB is constructed properly
    Moved the newHeader and prevHeader check with commonHeader to the top of
    the for loops because in the case of newHeader or prevHeader being equal
    to the commonHeader, we were deleting all the entries in the DB prior to
    commonHeader.

--- core/headerchain.go
+++ core/headerchain.go
@@ -244,6 +244,9 @@ func (hc *HeaderChain) SetCurrentHeader(head *types.Header) error {
 	// Accumulate the hash slice stack
 	var hashStack []*types.Header
 	for {
+		if newHeader.Hash() == commonHeader.Hash() {
+			break
+		}
 		hashStack = append(hashStack, newHeader)
 		newHeader = hc.GetHeader(newHeader.ParentHash(), newHeader.NumberU64()-1)
 
@@ -251,22 +254,17 @@ func (hc *HeaderChain) SetCurrentHeader(head *types.Header) error {
 		if newHeader.Hash() == hc.config.GenesisHash {
 			break
 		}
-
-		if newHeader.Hash() == commonHeader.Hash() {
-			break
-		}
 	}
 
 	for {
+		if prevHeader.Hash() == commonHeader.Hash() {
+			break
+		}
 		rawdb.DeleteCanonicalHash(hc.headerDb, prevHeader.NumberU64())
 		prevHeader = hc.GetHeader(prevHeader.ParentHash(), prevHeader.NumberU64()-1)
 
 		// genesis check to not delete the genesis block
 		if prevHeader.Hash() == hc.config.GenesisHash {
-			break
-		}
-
-		if prevHeader.Hash() == commonHeader.Hash() {
 			break
 		}
 	}
