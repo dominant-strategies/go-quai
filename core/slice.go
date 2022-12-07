dominant-strategies/go-quai

commit ad9b6dfcff1e1eff64202b86e6c3cdca68874515

Added location guard on SubRelay on PH send

core/slice.go
@@ -191,7 +191,7 @@ func (sl *Slice) Append(header *types.Header, domPendingHeader *types.Header, do
 	updateMiner := sl.pickPhCacheHead(reorg, pendingHeaderWithTermini)
 
 	// Relay the new pendingHeader
-	sl.relayPh(pendingHeaderWithTermini, updateMiner, reorg, domOrigin)
+	sl.relayPh(pendingHeaderWithTermini, updateMiner, reorg, domOrigin, block.Location())
 
 	// Remove the header from the future headers cache
 	sl.futureHeaders.Remove(block.Hash())
@@ -208,7 +208,7 @@ func (sl *Slice) Append(header *types.Header, domPendingHeader *types.Header, do
 }
 
 // relayPh sends pendingHeaderWithTermini to subordinates
-func (sl *Slice) relayPh(pendingHeaderWithTermini types.PendingHeader, updateMiner bool, reorg bool, domOrigin bool) {
+func (sl *Slice) relayPh(pendingHeaderWithTermini types.PendingHeader, updateMiner bool, reorg bool, domOrigin bool, location common.Location) {
 	nodeCtx := common.NodeLocation.Context()
 
 	if nodeCtx == common.ZONE_CTX {
@@ -219,7 +219,7 @@ func (sl *Slice) relayPh(pendingHeaderWithTermini types.PendingHeader, updateMin
 		}
 	} else if !domOrigin {
 		for i := range sl.subClients {
-			sl.subClients[i].SubRelayPendingHeader(context.Background(), pendingHeaderWithTermini, reorg)
+			sl.subClients[i].SubRelayPendingHeader(context.Background(), pendingHeaderWithTermini, reorg, location)
 		}
 	}
 }
@@ -325,27 +325,35 @@ func (sl *Slice) GetPendingHeader() (*types.Header, error) {
 }
 
 // SubRelayPendingHeader takes a pending header from the sender (ie dominant), updates the phCache with a composited header and relays result to subordinates
-func (sl *Slice) SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg bool) {
+func (sl *Slice) SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg bool, location common.Location) {
 	sl.phCachemu.Lock()
 	defer sl.phCachemu.Unlock()
 	nodeCtx := common.NodeLocation.Context()
 
 	if nodeCtx == common.REGION_CTX {
-		err := sl.updatePhCacheFromDom(pendingHeader, common.NodeLocation.Region(), []int{common.PRIME_CTX}, reorg)
-		if err != nil {
-			return
+		// Adding a guard on the region that was already updated in the synchronous path.
+		if location.Region() != common.NodeLocation.Region() {
+			err := sl.updatePhCacheFromDom(pendingHeader, common.NodeLocation.Region(), []int{common.PRIME_CTX}, reorg)
+			if err != nil {
+				return
+			}
 		}
 		for i := range sl.subClients {
-			sl.subClients[i].SubRelayPendingHeader(context.Background(), sl.phCache[pendingHeader.Termini[common.NodeLocation.Region()]], reorg)
+			sl.subClients[i].SubRelayPendingHeader(context.Background(), sl.phCache[pendingHeader.Termini[common.NodeLocation.Region()]], reorg, location)
 		}
 	} else {
-		err := sl.updatePhCacheFromDom(pendingHeader, common.NodeLocation.Zone(), []int{common.PRIME_CTX, common.REGION_CTX}, reorg)
-		if err != nil {
-			return
-		}
-		bestPh, exists := sl.phCache[sl.pendingHeaderHeadHash]
-		if exists {
-			sl.miner.worker.pendingHeaderFeed.Send(bestPh.Header)
+		// This check prevents a double send to the miner.
+		// If the previous block on which the given pendingHeader was built is the same as the NodeLocation
+		// the pendingHeader update has already been sent to the miner for the given location in relayPh.
+		if location.Equal(common.NodeLocation) {
+			err := sl.updatePhCacheFromDom(pendingHeader, common.NodeLocation.Zone(), []int{common.PRIME_CTX, common.REGION_CTX}, reorg)
+			if err != nil {
+				return
+			}
+			bestPh, exists := sl.phCache[sl.pendingHeaderHeadHash]
+			if exists {
+				sl.miner.worker.pendingHeaderFeed.Send(bestPh.Header)
+			}
 		}
 	}
 }

core/core.go
@@ -106,8 +106,8 @@ func (c *Core) ConstructLocalBlock(header *types.Header) *types.Block {
 	return c.sl.ConstructLocalBlock(header)
 }
 
-func (c *Core) SubRelayPendingHeader(slPendingHeader types.PendingHeader, reorg bool) {
-	c.sl.SubRelayPendingHeader(slPendingHeader, reorg)
+func (c *Core) SubRelayPendingHeader(slPendingHeader types.PendingHeader, reorg bool, location common.Location) {
+	c.sl.SubRelayPendingHeader(slPendingHeader, reorg, location)
 }
 
 func (c *Core) GetPendingHeader() (*types.Header, error) {

eth/api_backend.go
@@ -374,8 +374,8 @@ func (b *QuaiAPIBackend) PendingBlockBody(hash common.Hash) *types.Body {
 	return b.eth.core.PendingBlockBody(hash)
 }
 
-func (b *QuaiAPIBackend) SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg bool) {
-	b.eth.core.SubRelayPendingHeader(pendingHeader, reorg)
+func (b *QuaiAPIBackend) SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg bool, location common.Location) {
+	b.eth.core.SubRelayPendingHeader(pendingHeader, reorg, location)
 }
 
 func (b *QuaiAPIBackend) GetPendingHeader() (*types.Header, error) {

internal/quaiapi/backend.go
@@ -77,7 +77,7 @@ type Backend interface {
 	InsertBlock(ctx context.Context, block *types.Block) (int, error)
 	PendingBlock() *types.Block
 	PendingBlockBody(hash common.Hash) *types.Body
-	SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg bool)
+	SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg bool, location common.Location)
 	GetPendingHeader() (*types.Header, error)
 	PendingBlockAndReceipts() (*types.Block, types.Receipts)
 

internal/quaiapi/quai_api.go
@@ -561,7 +561,7 @@ func (s *PublicBlockChainQuaiAPI) SubRelayPendingHeader(ctx context.Context, raw
 		return
 	}
 	pendingHeader := types.PendingHeader{Header: subRelay.Header, Termini: subRelay.Termini}
-	s.b.SubRelayPendingHeader(pendingHeader, subRelay.Reorg)
+	s.b.SubRelayPendingHeader(pendingHeader, subRelay.Reorg, subRelay.Location)
 }
 
 func (s *PublicBlockChainQuaiAPI) GetPendingHeader(ctx context.Context) (map[string]interface{}, error) {

quaiclient/quaiclient.go
@@ -153,10 +153,11 @@ func (ec *Client) Append(ctx context.Context, header *types.Header, domPendingHe
 	return nil
 }
 
-func (ec *Client) SubRelayPendingHeader(ctx context.Context, pendingHeader types.PendingHeader, reorg bool) {
+func (ec *Client) SubRelayPendingHeader(ctx context.Context, pendingHeader types.PendingHeader, reorg bool, location common.Location) {
 	data := map[string]interface{}{"Header": RPCMarshalHeader(pendingHeader.Header)}
 	data["Termini"] = pendingHeader.Termini
 	data["Reorg"] = reorg
+	data["Location"] = location
 
 	ec.c.CallContext(ctx, nil, "quai_subRelayPendingHeader", data)
 

commit ba2a2ce8fdc71862dd3e1f299536744072450faa

bugfix: update when location not equal in zone
This bug was introduced in ad9b6dfc.

core/slice.go
@@ -345,7 +345,7 @@ func (sl *Slice) SubRelayPendingHeader(pendingHeader types.PendingHeader, reorg 
 		// This check prevents a double send to the miner.
 		// If the previous block on which the given pendingHeader was built is the same as the NodeLocation
 		// the pendingHeader update has already been sent to the miner for the given location in relayPh.
-		if location.Equal(common.NodeLocation) {
+		if !location.Equal(common.NodeLocation) {
 			err := sl.updatePhCacheFromDom(pendingHeader, common.NodeLocation.Zone(), []int{common.PRIME_CTX, common.REGION_CTX}, reorg)
 			if err != nil {
 				return