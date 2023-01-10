87b382324952dd92e54f91e8ae638f5d7bb8c811
gop <gameofpointers@proton.me>
2023-01-10 11:52:52 -0600

    Prints to know which part of body is invalid

--- eth/protocols/eth/handlers.go
+++ eth/protocols/eth/handlers.go
@@ -273,17 +273,17 @@ func handleNewBlock(backend Backend, msg Decoder, peer *Peer) error {
 		return nil // TODO(karalabe): return error eventually, but wait a few releases
 	}
 	if hash := types.DeriveSha(ann.Block.Transactions(), trie.NewStackTrie(nil)); hash != ann.Block.TxHash() {
-		log.Warn("Propagated block has invalid body", "have", hash, "exp", ann.Block.TxHash())
+		log.Warn("Propagated block has invalid body(tx)", "have", hash, "exp", ann.Block.TxHash())
 		return nil // TODO(karalabe): return error eventually, but wait a few releases
 	}
 	if hash := types.DeriveSha(ann.Block.ExtTransactions(), trie.NewStackTrie(nil)); hash != ann.Block.EtxHash() {
-		log.Warn("Propagated block has invalid body", "have", hash, "exp", ann.Block.EtxHash())
+		log.Warn("Propagated block has invalid body(etx)", "have", hash, "exp", ann.Block.EtxHash())
 		return nil // TODO(karalabe): return error eventually, but wait a few releases
 	}
 	// Dom nodes need to validate the subordinate manifest against the subordinate's manifesthash
 	if nodeCtx < common.ZONE_CTX {
 		if hash := types.DeriveSha(ann.Block.SubManifest(), trie.NewStackTrie(nil)); hash != ann.Block.ManifestHash(nodeCtx+1) {
-			log.Warn("Propagated block has invalid body", "have", hash, "exp", ann.Block.ManifestHash())
+			log.Warn("Propagated block has invalid body(submanifest)", "have", hash, "exp", ann.Block.ManifestHash())
 			return nil // TODO(karalabe): return error eventually, but wait a few releases
 		}
 	}
