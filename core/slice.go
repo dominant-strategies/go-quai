40d6024e0d7dc65b38b6955bcd13e01af94e4874
gop <gameofpointers@proton.me>
2023-01-12 10:39:09 -0600

    DROP this commit: preventing crash on domRelayPendingEtxs

--- core/slice.go
+++ core/slice.go
@@ -89,6 +89,7 @@ func NewSlice(db ethdb.Database, config *Config, txConfig *TxPoolConfig, isLocal
 	if nodeCtx != common.ZONE_CTX {
 		sl.subClients = makeSubClients(subClientUrls)
 	}
+
 	// only set domClient if the chain is not Prime.
 	if nodeCtx != common.PRIME_CTX {
 		go func() {
@@ -112,16 +113,18 @@ func (sl *Slice) Append(header *types.Header, domPendingHeader *types.Header, do
 	location := header.Location()
 	isDomCoincident := sl.engine.IsDomCoincident(header)
 
-	if sl.domClient == nil && nodeCtx != common.PRIME_CTX {
-		if header.NumberU64() == 3 && nodeCtx == common.REGION_CTX || header.NumberU64() == 1 && nodeCtx == common.ZONE_CTX {
-			return nil, ErrSubNotSyncedToDom
-		}
-	}
-
 	// Don't append the block which already exists in the database.
 	if sl.hc.HasHeader(header.Hash(), header.NumberU64()) {
 		log.Warn("Block has already been appended: ", "Hash: ", header.Hash())
 		return nil, ErrKnownBlock
+	}
+
+	// This is to prevent a crash when we try to insert blocks before domClient is on.
+	// Ideally this check should not exist here and should be fixed before we start the slice.
+	if sl.domClient == nil && nodeCtx != common.PRIME_CTX {
+		if header.NumberU64() > 3 && nodeCtx == common.REGION_CTX || header.NumberU64() > 1 && nodeCtx == common.ZONE_CTX {
+			return nil, ErrSubNotSyncedToDom
+		}
 	}
 
 	// Construct the block locally
