464de70087738a117e88fd11a719e10c9625e30a
wizeguyy <wizeguyy@dominantstrategies.io>
2023-01-19 11:41:54 -0600

    Re-enable subordinate manifest & rollup checks

--- core/slice.go
+++ core/slice.go
@@ -298,6 +298,9 @@ func (sl *Slice) CollectSubRollups(b *types.Block) ([]types.Transactions, error)
 				subRollups[ctx] = append(subRollups[ctx], pendingEtxs[ctx]...)
 			}
 		}
+		if subRollupHash := types.DeriveSha(subRollups[nodeCtx+1], trie.NewStackTrie(nil)); subRollupHash != b.EtxRollupHash(nodeCtx+1) {
+			return nil, errors.New("sub rollup does not match sub rollup hash")
+		}
 	}
 	return subRollups, nil
 }
