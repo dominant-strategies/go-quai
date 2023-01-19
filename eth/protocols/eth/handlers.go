310e45d9250818644c55935967a5a954b5e26c0c
wizeguyy <wizeguyy@dominantstrategies.io>
2023-01-19 11:41:54 -0600

    Re-enable subordinate manifest & rollup checks

--- eth/protocols/eth/handlers.go
+++ eth/protocols/eth/handlers.go
@@ -255,6 +255,7 @@ func handleNewBlockhashes(backend Backend, msg Decoder, peer *Peer) error {
 }
 
 func handleNewBlock(backend Backend, msg Decoder, peer *Peer) error {
+	nodeCtx := common.NodeLocation.Context()
 	// Retrieve and decode the propagated block
 	ann := new(NewBlockPacket)
 	if err := msg.Decode(ann); err != nil {
@@ -274,6 +275,13 @@ func handleNewBlock(backend Backend, msg Decoder, peer *Peer) error {
 	if hash := types.DeriveSha(ann.Block.ExtTransactions(), trie.NewStackTrie(nil)); hash != ann.Block.EtxHash() {
 		log.Warn("Propagated block has invalid external transaction", "have", hash, "exp", ann.Block.EtxHash())
 		return nil // TODO(karalabe): return error eventually, but wait a few releases
+	}
+	// Dom nodes need to validate the subordinate manifest against the subordinate's manifesthash
+	if nodeCtx < common.ZONE_CTX {
+		if hash := types.DeriveSha(ann.Block.SubManifest(), trie.NewStackTrie(nil)); hash != ann.Block.ManifestHash(nodeCtx+1) {
+			log.Warn("Propagated block has invalid subordinate manifest", "have", hash, "exp", ann.Block.ManifestHash())
+			return nil // TODO(karalabe): return error eventually, but wait a few releases
+		}
 	}
 	ann.Block.ReceivedAt = msg.Time()
 	ann.Block.ReceivedFrom = peer
