344112d945666c24b52db9857857188b8abced06
Connor Hubbard <connor@dominantstrategies.io>
2023-05-16 12:30:23 -0500

    updated colosseum GasLimit and gensis hash

--- core/genesis.go
+++ core/genesis.go
@@ -320,7 +320,7 @@ func DefaultColosseumGenesisBlock() *Genesis {
 		Config:     params.ColosseumChainConfig,
 		Nonce:      66,
 		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
-		GasLimit:   15000000,
+		GasLimit:   50000000,
 		Difficulty: big.NewInt(2048576),
 	}
 }
--- params/config.go
+++ params/config.go
@@ -25,7 +25,7 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	ColosseumGenesisHash = common.HexToHash("0x98e42a430780db6a01cf98ce6a199f45716b105df8bb9c003a0f09f5254b5768")
+	ColosseumGenesisHash = common.HexToHash("0x6b3c28921a94aa0b17240fe8144e7936c870069143195438f2360521cb73f5a1")
 	GardenGenesisHash    = common.HexToHash("0x2b57b896bb0522a0b5e15fc68e0302eb3c514e47b06ab2a4de16c4bffd2ceeed")
 	OrchardGenesisHash   = common.HexToHash("0xb998fb9bf1e027f09efef05fdb6276a31156068592e6a989348ffb4c0d4387f7")
 	LocalGenesisHash     = common.HexToHash("0x4c35b1216decc6aa2431fa2d2a1c68f15d70f83a309041ed1bfef5ad6592a3d4")
