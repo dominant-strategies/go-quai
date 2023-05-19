3e7df368c270ef165d02ad83c5b91592aa4c64d3
Connor Hubbard <connor@dominantstrategies.io>
2023-05-19 16:46:09 -0500

    added shell script to insta pprof

---
+++ pprof.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Array of ports and corresponding names
+ports=("8081" "8090" "8100" "8110" "8091" "8092" "8093" "8101" "8102" "8103" "8111" "8112" "8113")
+names=("PRIME" "Region_0" "Region_1" "Region_2" "Zone_0-0" "Zone_0-1" "Zone_0-2" "Zone_1-0" "Zone_1-1" "Zone_1-2" "Zone_2-0" "Zone_2-1" "Zone_2-2")
+
+# Loop over the ports
+for i in ${!ports[@]}
+do
+    port=${ports[$i]}
+    name=${names[$i]}
+
+    # Run the go tool pprof command for each port
+    echo "Running pprof for port $port"
+    go tool pprof -svg http://localhost:$port/debug/pprof/goroutine > "$name"_$port.svg
+done
