a1d31ed52e421bacc835de3a9c136b96351b0ef3
gop <gameofpointers@proton.me>
2023-06-29 08:00:37 -0500

    Updated the galena gas limit to 80M and difficulty to 4M

--- params/config.go
+++ params/config.go
@@ -29,7 +29,7 @@ var (
 	GardenGenesisHash    = common.HexToHash("0x6210219b7847439325215a2d17182b801d9cc8efc2b0004d9511d37ef623405b")
 	OrchardGenesisHash   = common.HexToHash("0xc0fc30c7860b8f4ec7a8fe1a54dfcbca17167eca5bdcbdc077b6d50addf79763")
 	LocalGenesisHash     = common.HexToHash("0xb1eb552366e4083a11585418c4b193f024985d45b23103f0653416de083a6512")
-	GalenaGenesisHash    = common.HexToHash("0xac2b5abb66351e833123c3364283020cadb33df906252b0f86a081ae11e0fab9")
+	GalenaGenesisHash    = common.HexToHash("0x15b5146b4110caf25d69423cc0af2615192ecc2ba98a1de2f7bea81171155298")
 )
 
 var (
