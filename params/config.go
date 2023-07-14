df1b80aaee323bf820296332f9fb7bb4dbe2e820
Connor Hubbard <connor@dominantstrategies.io>
2023-07-14 11:02:22 -0500

    fixed genesis hash for colosseum

--- params/config.go
+++ params/config.go
@@ -25,7 +25,7 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	ColosseumGenesisHash = common.HexToHash("0xf4ccaa291097df67e2d98a1369955d14085f9241a5018d2bef532dc84c0039c6")
+	ColosseumGenesisHash = common.HexToHash("0xb1cba48a8d1ace79923edc4c03fc66a4990072b8c545783f94c85ad71d20b1e6")
 	GardenGenesisHash    = common.HexToHash("0x6210219b7847439325215a2d17182b801d9cc8efc2b0004d9511d37ef623405b")
 	OrchardGenesisHash   = common.HexToHash("0xc0fc30c7860b8f4ec7a8fe1a54dfcbca17167eca5bdcbdc077b6d50addf79763")
 	LocalGenesisHash     = common.HexToHash("0xae8a1dc375464487ed130283bc1f4e809ec4eb4ab61354c48ce50bb0f88c42bd")
