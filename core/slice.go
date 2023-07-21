7116931a343c15beb324b5df480450cc665e32c5
gop <gameofpointers@proton.me>
2023-07-21 13:51:59 -0500

    Moved the batch write to after the setcurrentheader

--- core/slice.go
+++ core/slice.go
@@ -265,18 +265,17 @@ func (sl *Slice) Append(header *types.Header, domPendingHeader *types.Header, do
 
 	}
 
-	// Append has succeeded write the batch
-	if err := batch.Write(); err != nil {
-		return nil, false, err
-	}
-
 	if subReorg {
 		log.Info("Choosing phHeader Append:", "NumberArray:", pendingHeaderWithTermini.Header.NumberArray(), "Number:", pendingHeaderWithTermini.Header.Number(), "ParentHash:", pendingHeaderWithTermini.Header.ParentHash(), "Terminus:", pendingHeaderWithTermini.Termini[c_terminusIndex])
 		sl.bestPhKey = pendingHeaderWithTermini.Termini[c_terminusIndex]
 		block.SetAppendTime(time.Duration(time9))
 		sl.hc.SetCurrentHeader(batch, block.Header())
 		sl.hc.chainHeadFeed.Send(ChainHeadEvent{Block: block})
+	}
 
+	// Append has succeeded write the batch
+	if err := batch.Write(); err != nil {
+		return nil, false, err
 	}
 
 	// Relay the new pendingHeader
