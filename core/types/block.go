08dffc6ae72ff68e2e1ef6517d8f68bc61107842
kiltsonfire <joe@dominantstrategies.io>
2023-07-26 10:08:08 -0500

    Added primeTermini array to Termini struct with helpers

--- core/types/block.go
+++ core/types/block.go
@@ -1104,6 +1104,9 @@ func CopyTermini(termini Termini) Termini {
 	for i, t := range termini.primeTermini {
 		newTermini.SetPrimeTerminiAtIndex(t, i)
 	}
+	for i, t := range termini.primeTermini {
+		newTermini.primeTermini[i] = t
+	}
 	return newTermini
 }
 
