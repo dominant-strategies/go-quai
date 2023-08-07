678a180eb30493863f94b3597377f80afe4ef9ab
gop <gameofpointers@proton.me>
2023-08-07 18:30:09 -0500

    bugfix: Fixed unncessary crashes on restart if pending header cache is not stored properly

--- core/core.go
+++ core/core.go
@@ -203,7 +203,9 @@ func (c *Core) serviceBlocks(hashNumberList []types.HashAndNumber) {
 				if parentBlockOrder < nodeCtx && c.GetHeaderByHash(parentBlock.Hash()) == nil {
 					log.Info("Requesting the dom to get the block if it doesnt have and try to append", "Hash", parentBlock.Hash(), "Order", parentBlockOrder)
 					// send a signal to the required dom to fetch the block if it doesnt have it, or its not in its appendqueue
-					go c.sl.domClient.RequestDomToAppendOrFetch(context.Background(), parentBlock.Hash(), parentBlockOrder)
+					if c.sl.domClient != nil {
+						go c.sl.domClient.RequestDomToAppendOrFetch(context.Background(), parentBlock.Hash(), parentBlockOrder)
+					}
 				}
 				c.InsertChain([]*types.Block{types.NewBlockWithHeader(block)})
 			} else {
@@ -230,7 +232,9 @@ func (c *Core) RequestDomToAppendOrFetch(hash common.Hash, order int) {
 		}
 	} else if nodeCtx == common.REGION_CTX {
 		if order < nodeCtx { // Prime block
-			go c.sl.domClient.RequestDomToAppendOrFetch(context.Background(), hash, order)
+			if c.sl.domClient != nil {
+				go c.sl.domClient.RequestDomToAppendOrFetch(context.Background(), hash, order)
+			}
 		}
 		_, exists := c.appendQueue.Get(hash)
 		if !exists {
@@ -351,7 +355,9 @@ func (c *Core) WriteBlock(block *types.Block) {
 			c.addToAppendQueue(block)
 			// If a dom block comes in and we havent appended it yet
 		} else if order < nodeCtx && c.GetHeaderByHash(block.Hash()) == nil {
-			go c.sl.domClient.RequestDomToAppendOrFetch(context.Background(), block.Hash(), order)
+			if c.sl.domClient != nil {
+				go c.sl.domClient.RequestDomToAppendOrFetch(context.Background(), block.Hash(), order)
+			}
 		}
 	}
 	if c.sl.hc.GetHeaderOrCandidateByHash(block.Hash()) == nil { //This must have termini need to use getheaderorcandidatebyhash
