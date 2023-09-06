fbb2dac9a74f8d846783e4df5e5181e1c7b7f540
Hussam <hussam@dominantstrategies.io>
2023-09-06 16:10:14 -0500

    ExtraData for Colosseum

--- params/config.go
+++ params/config.go
@@ -26,7 +26,7 @@ import (
 // Genesis hashes to enforce below configs on.
 var (
 	// Progpow GenesisHashes
-	ProgpowColosseumGenesisHash  = common.HexToHash("0x6053d0826a42d11d44a2172d309809aa6abb97bf7cdc24a3dd8be81ee6528468")
+	ProgpowColosseumGenesisHash  = common.HexToHash("0x115e8e34985de543f91b27f1efb42f387a6ff5d43ac6dc612276eadeabf92518")
 	ProgpowGardenGenesisHash     = common.HexToHash("0x3725ae59140167e31e4d4457e9948d716c687ac3a4b278350137356ee73060d1")
 	ProgpowOrchardGenesisHash    = common.HexToHash("0xeb625504882304b641551f8f33706cc4a7009b14325fa52af92274032504c46e")
 	ProgpowLocalGenesisHash      = common.HexToHash("0x7f594e88d6c93a00680840a855b7c9349218751cc344562dacb43a63eec02d93")
