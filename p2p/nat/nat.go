c1ed25300256159925f948aebc57b9633dffe767
wizeguyy <wizeguyy@dominantstrategies.io>
2023-09-21 09:19:24 -0500

    Improve docs link in UPnP error message

--- p2p/nat/nat.go
+++ p2p/nat/nat.go
@@ -154,8 +154,8 @@ func Any() Interface {
 				return c
 			}
 		}
-		log.Error("This computer is not publicly recahable and cannot join the P2P network.")
-		fmt.Println("\nPlease forward the necessary ports or enable UPnP on your router. See https://docs.quai.network for guidance.")
+		log.Error("This computer is not publicly reachable and cannot join the P2P network.")
+		fmt.Println("\nPlease forward the necessary ports or enable UPnP on your router. See https://docs.quai.network/node/node-overview/run-a-node#router-configuration for guidance.")
 		os.Exit(1)
 		return nil
 	})
