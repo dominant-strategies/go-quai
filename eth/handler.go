3443fb770011d4e79a14b26952011704b9a659f6
gop <gameofpointers@proton.me>
2023-10-08 10:33:36 -0500

    Entropy based stale broadcast decision

--- eth/handler.go
+++ eth/handler.go
@@ -19,6 +19,7 @@ package eth
 import (
 	"errors"
 	"math"
+	"math/big"
 	"math/rand"
 	"sync"
 	"sync/atomic"
@@ -170,6 +171,19 @@ func newHandler(config *handlerConfig) (*handler, error) {
 	heighter := func() uint64 {
 		return h.core.CurrentHeader().NumberU64()
 	}
+	currentThresholdS := func() *big.Int {
+		return h.core.Engine().IntrinsicLogS(h.core.CurrentHeader().Hash())
+	}
+	currentS := func() *big.Int {
+		// This is not the TotalLogS of the current header and instead the parent
+		// entropy. Calculating the TotalLogS is very expensive as it requires
+		// multiple floating point additions and multiplications and also few
+		// logarithmic calculations for IntrinsicLogS. Instead we can use the
+		// ParentEntropy which is only off by one block in most cases and also
+		// since the use of this currentS is for approximations of the stale
+		// broadcast distance only, ParentEntropy should suffice
+		return h.core.CurrentHeader().ParentEntropy()
+	}
 	// writeBlock writes the block to the DB
 	writeBlock := func(block *types.Block) {
 		if nodeCtx == common.ZONE_CTX && block.NumberU64()-1 == h.core.CurrentHeader().NumberU64() && h.core.ProcessingState() {
@@ -179,7 +193,7 @@ func newHandler(config *handlerConfig) (*handler, error) {
 		}
 		h.core.WriteBlock(block)
 	}
-	h.blockFetcher = fetcher.NewBlockFetcher(h.core.GetBlockOrCandidateByHash, writeBlock, validator, h.BroadcastBlock, heighter, h.removePeer, h.core.IsBlockHashABadHash)
+	h.blockFetcher = fetcher.NewBlockFetcher(h.core.GetBlockOrCandidateByHash, writeBlock, validator, h.BroadcastBlock, heighter, currentThresholdS, currentS, h.removePeer, h.core.IsBlockHashABadHash)
 
 	// Only initialize the Tx fetcher in zone
 	if nodeCtx == common.ZONE_CTX && h.core.ProcessingState() {
