4e5ac3c716515f82d306587ee5ff13f7268ca6f6
Alejo Acosta <alejoacos@gmail.com>
2023-10-17 00:13:11 -0300

    add support for node info persistence on file

--- p2p/node/cid.go
+++ p2p/node/cid.go
@@ -1,12 +1,18 @@
 package node
 
 import (
+	"bufio"
 	"crypto/rand"
 	"os"
 
 	"github.com/dominant-strategies/go-quai/log"
 
 	"github.com/libp2p/go-libp2p/core/crypto"
+)
+
+const (
+	// file to store node's CID and listening address
+	nodeInfoFile = "node.info"
 )
 
 // Returns the private key stored in the file to be used to generate
@@ -51,4 +57,36 @@ func getPrivKey(privKeyFile string) (crypto.PrivKey, error) {
 	}
 	return privateKey, nil
 
+}
+
+// Utility function that synchronously writes the provided "info" string to the node.info file.
+// If the file doesn't exist, it creates it. Otherwise, it appends the new "info" as a new line.
+func saveNodeInfo(info string) {
+	go func() {
+		// Open file with O_APPEND flag to append data to the file or create the file if it doesn't exist.
+		f, err := os.OpenFile(nodeInfoFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Errorf("error opening node info file: %s", err)
+			return
+		}
+		defer f.Close()
+
+		// Use bufio for efficient writing
+		writer := bufio.NewWriter(f)
+		defer writer.Flush()
+
+		// Append new line and write to file
+		writer.WriteString(info + "\n")
+	}()
+}
+
+// utility function used to delete any existing node info file
+func deleteNodeInfoFile() error {
+	if _, err := os.Stat(nodeInfoFile); !os.IsNotExist(err) {
+		err := os.Remove(nodeInfoFile)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
