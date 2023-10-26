3f48c2b0e9c1f6ebbdd07daaaabc3db9b7b1b0f3
gop <gameofpointers@proton.me>
2023-10-26 10:53:38 -0500

    Changed the protocol version number for Quai1

--- eth/protocols/eth/protocol.go
+++ eth/protocols/eth/protocol.go
@@ -30,7 +30,7 @@ import (
 
 // Constants to match up protocol versions and messages
 const (
-	QUAI1 = 100
+	QUAI1 = 101
 )
 
 // ProtocolName is the official short name of the `quai` protocol used during
