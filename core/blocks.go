fd6940202289615733cca1335502380ed60155a5
gop <gameofpointers@proton.me>
2023-10-29 21:20:58 -0500

    New fork: Recovery from the crash in Zone-0-0 due to invalid state
    * Updated the BadHashes for the new hard fork
    * Changed the Quai protocol version to reduce the noise after the fork

--- core/blocks.go
+++ core/blocks.go
@@ -26,27 +26,27 @@ type HeirarchyBadHashes struct {
 
 var BadHashes = []HeirarchyBadHashes{
 	HeirarchyBadHashes{
-		PrimeContext: common.HexToHash("0x70ad991983e04ecc480218c5f19c67d089728800d9c59385c26138a0e05a4338"),
+		PrimeContext: common.HexToHash("0x03f10030572fe5b9d257bb799937099faea80f1f157c757f8a15da4c3e39cbd0"),
 		RegionContext: []common.Hash{
-			common.HexToHash("0x4d3211f5bc7301ad589cf70391a51c513e7b07de5295ca2bffc31337b32f2366"),
-			common.HexToHash("0x56b038de63672ea81e1f61ad8595d7960a2d4d67ae27126ddecd841104064a67"),
-			common.HexToHash("0xc491eff2156db47e42d6bcf569db8dadbd51d0d0e10646941194ddfd92b18f26"),
+			common.HexToHash("0x3da90f4c5f6584f564dcee484fcb3d07ad7acdd3be5e2f17e751ebc872440a36"),
+			common.HexToHash("0xda2197e8719a2350fba2a38392e77a453319f668eac5035a5fd1d133df9a014a"),
+			common.HexToHash("0xc53ff2a55af00b3915ad46c12d1b418e4f92de4d3663fa1ce152a8e4a7a7c086"),
 		},
 		ZoneContext: [][]common.Hash{
 			[]common.Hash{
-				common.HexToHash("0x99a0e419913cafa126e3ac80d0d176f02267e6bec6edee82c1fee0fad85b3ca8"),
-				common.HexToHash("0x3c8dff9ad5926316e2f71fb95efa35808d52fb392f35c384f5382515a44856bf"),
-				common.HexToHash("0xa3361dd358cd3373b1e7a8be1bf2c41a119c3d01314072c4c4f6626f4c0b88a7"),
+				common.HexToHash("0xbbbcddc758135c6ce9a982a7e49fce9174a34b5cb8b9e2ed17026ac309267960"),
+				common.HexToHash("0x62a58e2bae1c869f5c67d0d04718a2dd37dbfa462968be3c779ebeaa501ba4f2"),
+				common.HexToHash("0x9a35566465f8bc47b8ce904fbbdf4c33d230cb31021b9825f262d5280f14ab97"),
 			},
 			[]common.Hash{
-				common.HexToHash("0x7e3eef3ded1d56b938e040ec94176dcd047b675763441422bdab866dc9f5cc19"),
-				common.HexToHash("0xafc9b3464e2889f683b1c982e2dba68cc325016fc9540cbe4d770a9e609c86aa"),
-				common.HexToHash("0xeb38f5a1ffdd12a937e5b4ce16c0328b4d15137301aaf32959ba7493cf0ddb12"),
+				common.HexToHash("0x964615a38fc0caefeeb17688e9df01d19f9d559408e2bb80b4158093ec9bb780"),
+				common.HexToHash("0xabcf5d688d094a5108419dac68e9dfec9aabf3ba9944080e2eed518b4701bcf8"),
+				common.HexToHash("0x0172bc291bab64540cd8692cbcd63453dd360edfa96986ad7754e6111b299206"),
 			},
 			[]common.Hash{
-				common.HexToHash("0x69d02a02e0127b90013392d820e04d26c19a61d2ba057bf86832e1d4ee74ce75"),
-				common.HexToHash("0xcb1930e1215e015fa3da8ff3e587fe94c10fb4d1ea4a25c3339ccda677507aa7"),
-				common.HexToHash("0x20874acb12eb1da946f308ae56c06b60a5234f3eb4373f3a8c5b56339ad46370"),
+				common.HexToHash("0x351a335d9775bcf319b3a27ef3aee7db5b8b6d1610308b325d8e006b74d4feb2"),
+				common.HexToHash("0x7972d8c316c04eb059e7de85bc9b26b72c6a8bde66add94dc4781de336f3f7e4"),
+				common.HexToHash("0x366e9f2ff94d4ba1dbd616805437c10eeae33d52726c83f9ddf3ad450c5bb6ec"),
 			},
 		},
 	},
