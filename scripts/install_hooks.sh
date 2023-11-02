5d2c6f45fb2d8fc6369668a77f78571cf6a8da23
wizeguyy <wizeguyy@dominantstrategies.io>
2023-11-02 13:53:54 -0500

    Tweak hook install script to use relative paths

--- scripts/install_hooks.sh
+++ scripts/install_hooks.sh
@@ -5,7 +5,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 HOOKS_DIR="$REPO_ROOT/hooks"
 
 # Create symbolic links for each hook
-ln -sf "$HOOKS_DIR/pre-commit" "$REPO_ROOT/.git/hooks/pre-commit"
+ln -sf "../../scripts/hooks/pre-commit" "$REPO_ROOT/.git/hooks/pre-commit"
 
 # Make sure hooks are executable
 chmod +x "$REPO_ROOT/.git/hooks/pre-commit"
