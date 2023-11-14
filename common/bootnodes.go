4cb04d3e15006fb7509cf8bd770f314f198d2cb3
wizeguyy <wizeguyy@dominantstrategies.io>
2023-11-14 12:07:05 -0600

    Fix bootnode public key

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -4,7 +4,7 @@ var (
 
 	// ! NOTE: using public bootstrap peers only for testing purposes !
 	BootstrapPeers = []string{
-		"/ip4/35.232.36.100/tcp/4001/p2p/12D3KooWBxA6wb6ea87CreaHp6RKAv8MupVBnTxgCyyDJztvyB3h",
+		"/ip4/35.232.36.100/tcp/4001/p2p/12D3KooWKa7MFEtQx3CayUHdE31xzaDgurzhaBH9MKaGw4x3n9RN",
 		"/ip4/35.223.198.14/tcp/4001/p2p/12D3KooWDWPz7ghZzBXfygv5Q62ijk3XFZYYjgSXfQkXgQPBGtA8",
 	}
 )
