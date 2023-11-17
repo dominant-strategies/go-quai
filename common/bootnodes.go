55e772b2aa7c7910493749e56091dffdacee7f3b
Alejo Acosta <alejoacos@gmail.com>
2023-11-16 18:35:40 -0600

    add a 3rd bootstrap node

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -1,10 +1,9 @@
 package common
 
 var (
-
-	// ! NOTE: using public bootstrap peers only for testing purposes !
 	BootstrapPeers = []string{
 		"/ip4/35.232.36.100/tcp/4001/p2p/12D3KooWKa7MFEtQx3CayUHdE31xzaDgurzhaBH9MKaGw4x3n9RN",
 		"/ip4/35.223.198.14/tcp/4001/p2p/12D3KooWDWPz7ghZzBXfygv5Q62ijk3XFZYYjgSXfQkXgQPBGtA8",
+		"/ip4/34.31.144.9/tcp/4001/p2p/12D3KooWAsr23smoj51VxUrLM7LvjSjCvwzarB6DMaa5eAcs9qeH",
 	}
 )
