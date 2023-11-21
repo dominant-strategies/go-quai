e11faa26d65349aa4a0848b73c7f600aa13fd2a6
Alejo Acosta <alejoacos@gmail.com>
2023-11-21 13:15:23 -0300

    add 'send' subcommand for testing stream and quai protocol

---
+++ cmd/send.go
@@ -0,0 +1,198 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"bufio"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/dominant-strategies/go-quai/cmd/options"
+	"github.com/dominant-strategies/go-quai/common"
+	"github.com/dominant-strategies/go-quai/log"
+	"github.com/dominant-strategies/go-quai/p2p/node"
+	quaiprotocol "github.com/dominant-strategies/go-quai/p2p/protocol"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// In your cmd package where you define Cobra commands
+
+var sendCmd = &cobra.Command{
+	Use:          "send",
+	Short:        "Send a message using the Quai protocol",
+	Long:         `Send a message to a specific peer using the Quai protocol for testing purposes.`,
+	RunE:         runSend,
+	PreRunE:      sendCmdPreRun,
+	SilenceUsage: true,
+}
+
+var targetPeer string
+var message string
+var sendInterval time.Duration
+var protocolID string
+
+func init() {
+	startCmd.AddCommand(sendCmd)
+
+	sendCmd.Flags().StringVar(&targetPeer, "target", "", "Target peer multiaddress. If not provided, will use the default bootnode.")
+	sendCmd.Flags().StringVarP(&message, "message", "m", "", "Message to send")
+	sendCmd.Flags().DurationVar(&sendInterval, "interval", 0, "Interval between messages, i.e. '5s' (Set to 0 for a one-time send).")
+	// add flag to read the protocol id from the command line
+	sendCmd.Flags().StringVar(&protocolID, "protocol", quaiprotocol.ProtocolVersion, "Protocol ID, i.e. '/quai/1.0.0'")
+
+	sendCmd.MarkFlagRequired("message") // Ensure that message flag is provided
+}
+
+func sendCmdPreRun(cmd *cobra.Command, args []string) error {
+	// duplicated from cmd/start.go
+	// set keyfile path
+	if "" == viper.GetString(options.KEYFILE) {
+		configDir := cmd.Flag(options.CONFIG_DIR).Value.String()
+		viper.Set(options.KEYFILE, configDir+"private.key")
+	}
+
+	// if no bootstrap peers are provided, use the default ones defined in config/bootnodes.go
+	if bootstrapPeers := viper.GetStringSlice(options.BOOTPEERS); len(bootstrapPeers) == 0 {
+		log.Debugf("no bootstrap peers provided. Using default ones: %v", common.BootstrapPeers)
+		viper.Set(options.BOOTPEERS, common.BootstrapPeers)
+	}
+	return nil
+}
+
+func runSend(cmd *cobra.Command, args []string) error {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// create a new p2p node
+	node, err := node.NewNode(ctx)
+	if err != nil {
+		log.Fatalf("error creating node: %s", err)
+	}
+
+	// Ensure target peer is set, or use a default
+	if targetPeer == "" {
+		log.Warnf("no target peer provided. Using default bootnode: %s", common.BootstrapPeers[0])
+		targetPeer = common.BootstrapPeers[0]
+	}
+
+	// Convert targetPeer string to peer.AddrInfo
+	targetAddrInfo, err := peer.AddrInfoFromString(targetPeer)
+	if err != nil {
+		return err
+	}
+
+	// start node
+	if err := node.Start(); err != nil {
+		log.Fatalf("error starting node: %s", err)
+	}
+
+	// Connect to the target peer
+	if err := node.Host.Connect(ctx, *targetAddrInfo); err != nil {
+		log.Errorf("error connecting to target peer: %+v, error: %s", targetAddrInfo.ID, err)
+		return err
+	}
+
+	// Set the protocol id to use
+	if protocolID == "" {
+		protocolID = quaiprotocol.ProtocolVersion
+	}
+
+	// Function to send a message
+	sendMessage := func() error {
+		stream, err := node.Host.NewStream(ctx, targetAddrInfo.ID, protocol.ID(protocolID))
+		if err != nil {
+			log.Errorf("error opening stream: %s", err)
+			return err
+		}
+		defer stream.Close()
+
+		_, err = stream.Write([]byte(message + "\n"))
+		if err != nil {
+			log.Errorf("error writing to stream: %s", err)
+			return err
+		}
+
+		log.Debugf("Sent message: '%s'. Waiting for response...", message)
+
+		// Read the response from host2
+		buf := bufio.NewReader(stream)
+		// response, err := buf.ReadString('\n')
+		response, err := readWithTimeout(buf, ctx)
+		if err != nil {
+			log.Errorf("error reading from stream: %s", err)
+			return err
+		}
+		log.Debugf("Received response: '%s'", response)
+		return nil
+	}
+
+	if sendInterval > 0 {
+		// Send periodically
+		ticker := time.NewTicker(sendInterval)
+		defer ticker.Stop()
+		// wait for a SIGINT or SIGTERM signal
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		for {
+			select {
+			case <-ticker.C:
+				if err := sendMessage(); err != nil {
+					log.Errorf("Error sending message: %s", err)
+				}
+			case <-ch:
+				log.Warnf("Received 'stop' signal, shutting down gracefully...")
+				cancel()
+				if err := node.Stop(); err != nil {
+					panic(err)
+				}
+				log.Warnf("Node is offline")
+				return nil
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+		}
+	} else {
+		// Send once
+		return sendMessage()
+	}
+}
+
+// readWithTimeout reads from the bufio.Reader with a 5 seconds timeout.
+func readWithTimeout(buf *bufio.Reader, ctx context.Context) (string, error) {
+	resultChan := make(chan string)
+	errChan := make(chan error)
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	go func() {
+		response, err := buf.ReadString('\n')
+		if err != nil {
+			errChan <- err
+			return
+		}
+		resultChan <- response
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case response := <-resultChan:
+		return response, nil
+	case err := <-errChan:
+		return "", err
+
+	case <-ticker.C:
+		return "", errors.New("timeout waiting for response")
+	}
+}
