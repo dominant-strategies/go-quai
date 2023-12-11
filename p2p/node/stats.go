8a91f93d0a68b0440f377cac1c9f44aa31cab489
Hussam <hussam@dominantstrategies.io>
2023-12-11 14:18:11 -0600

    Add peer stats information for WAN+LAN

--- p2p/node/stats.go
+++ p2p/node/stats.go
@@ -8,19 +8,24 @@ import (
 
 // Returns the number of peers in the routing table, as well as how many active
 // connections we currently have.
-func (p *P2PNode) connectionStats() (int, int) {
-	routingTableSize := p.dht.RoutingTable().Size()
-	numConnected := len(p.Host.Network().Peers())
-	return routingTableSize, numConnected
+func (p *P2PNode) connectionStats() (int, int, int) {
+	WANPeerNum := len(p.dht.WAN.RoutingTable().ListPeers())
+	LANPeerNum := len(p.dht.LAN.RoutingTable().ListPeers())
+	peers := p.Host.Network().Peers()
+	numConnected := len(peers)
+
+	return WANPeerNum, LANPeerNum, numConnected
 }
 
 func (p *P2PNode) statsLoop() {
-	ticker := time.NewTicker(10 * time.Second)
+	ticker := time.NewTicker(30 * time.Second)
 	for {
 		select {
 		case <-ticker.C:
-			routingTableSize, numConnected := p.connectionStats()
-			log.Infof("Routing Table Size: %d, Number of Connected Peers: %d", routingTableSize, numConnected)
+			WANPeerNum, LANPeerNum, peersConnected := p.connectionStats()
+			
+			log.Debugf("Number of peers connected: %d", peersConnected)
+			log.Debugf("Peers in WAN Routing table: %d, Peers in LAN Routing table: %d", WANPeerNum, LANPeerNum)
 		case <-p.ctx.Done():
 			log.Warnf("Context cancelled. Stopping stats loop...")
 			return
