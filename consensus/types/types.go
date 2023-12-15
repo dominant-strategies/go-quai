479ff9146df88df7d597a8dc563a78a0626ed526
Hussam <hussam@dominantstrategies.io>
2023-12-15 11:51:19 -0600

    Add SliceID type and subscription suffixes

--- consensus/types/types.go
+++ consensus/types/types.go
@@ -1,7 +1,20 @@
 package types
 
+import (
+    "strconv"
+)
+
 // Hash represents a 256 bit hash
 type Hash [32]byte
 
 // Location describes the ontological location of a chain within the Quai hierarchy of blockchains
 type Location [2]byte
+
+type SliceID struct {
+    Region  int
+    Zone    int
+}
+
+func (sliceID SliceID) String() string {
+    return strconv.Itoa(sliceID.Region) + "." + strconv.Itoa(sliceID.Zone)
+}
--- p2p/types.go
+++ p2p/types.go
@@ -4,6 +4,12 @@ import (
 	"github.com/libp2p/go-libp2p/core"
 )
 
+const (
+	// Prefix for all block topics
+	C_blockTopicName = "block"
+	C_transactionTopicName = "transaction"
+)
+
 // Multiaddr aliases the Multiaddr type from github.com/libp2p/core
 //
 // Refer to the docs on that type for more info.
