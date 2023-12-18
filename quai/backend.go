5d2547084201204880f7657164d81794e28cacc6
gop <gameofpointers@proton.me>
2023-12-18 12:02:51 -0600

    Added Filter APIs to Quai namespace

--- quai/backend.go
+++ quai/backend.go
@@ -279,7 +279,7 @@ func (s *Quai) APIs() []rpc.API {
 			Service:   NewPrivateMinerAPI(s),
 			Public:    false,
 		}, {
-			Namespace: "eth",
+			Namespace: "quai",
 			Version:   "1.0",
 			Service:   filters.NewPublicFilterAPI(s.APIBackend, 5*time.Minute),
 			Public:    true,
