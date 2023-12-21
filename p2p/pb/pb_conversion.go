7cae3f6022a0e5399cdfe5b8dd716fd5c74e5e90
Hussam <hussam@dominantstrategies.io>
2023-12-21 12:09:24 -0600

    Move protobuf functions to pb package

--- p2p/pb/pb_conversion.go
+++ p2p/pb/pb_conversion.go
@@ -1,15 +1,14 @@
-package node
+package pb
 
 import (
 	"encoding/hex"
 
 	"github.com/dominant-strategies/go-quai/consensus/types"
-	"github.com/dominant-strategies/go-quai/p2p/pb"
 )
 
 // converts a custom go Block type (types.Block) to a protocol buffer Block type (pb.Block)
-func convertToProtoBlock(block types.Block) *pb.Block {
-	return &pb.Block{
+func ConvertToProtoBlock(block types.Block) *Block {
+	return &Block{
 		Hash: hex.EncodeToString(block.Hash[:]),
 		// ... map other fields
 	}
