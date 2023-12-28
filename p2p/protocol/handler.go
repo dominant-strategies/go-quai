1249ef85d9d9ae05c2fb70b7bf53e145d296123b
Alejo Acosta <alejoacos@gmail.com>
2023-12-28 15:29:25 -0300

    implement function to handle a block request via a stream

--- p2p/protocol/handler.go
+++ p2p/protocol/handler.go
@@ -1,7 +1,11 @@
 package protocol
 
 import (
+	"github.com/dominant-strategies/go-quai/common"
+	"github.com/dominant-strategies/go-quai/consensus/types"
 	"github.com/dominant-strategies/go-quai/log"
+	"github.com/dominant-strategies/go-quai/p2p/pb"
+	"github.com/gogo/protobuf/proto"
 	"github.com/libp2p/go-libp2p/core/network"
 )
 
@@ -17,11 +21,58 @@ func QuaiProtocolHandler(stream network.Stream, node QuaiP2PNode) {
 		return
 	}
 
-	if err := processJoinRequest(stream); err != nil {
-		log.Warnf("error processing join request: %s", err)
-		return
+	// Enter the read loop for the stream and handle messages
+	for {
+		data, err := common.ReadMessageFromStream(stream)
+		if err != nil {
+			log.Errorf("error reading message from stream: %s", err)
+			return
+		}
+
+		var protoMessage proto.Message
+		err = pb.UnmarshalProtoMessage(data, protoMessage)
+		if err != nil {
+			log.Errorf("error unmarshalling message: %s", err)
+			return
+		}
+
+		switch msg := protoMessage.(type) {
+		case *pb.BlockRequest:
+			// get the hash from the block request
+			blockReq := msg
+			hash, err := types.NewHashFromString(blockReq.Hash)
+			if err != nil {
+				log.Errorf("error converting hash from string: %s", err)
+				// TODO: handle error
+				return
+			}
+			// check if we have the block in our cache
+			block := node.GetBlock(hash)
+			if block == nil {
+				// TODO: handle block not found
+				log.Warnf("block not found in cache")
+				return
+			}
+			// convert the block to a protocol buffer and send it back to the peer
+			data, err := pb.MarshalBlock(block)
+			if err != nil {
+				log.Errorf("error marshalling block: %s", err)
+				// TODO: handle error
+				return
+			}
+			err = common.WriteMessageToStream(stream, data)
+			if err != nil {
+				log.Errorf("error writing message to stream: %s", err)
+				// TODO: handle error
+				return
+			}
+			log.Debugf("Sent block %s to peer %s", block.Hash, stream.Conn().RemotePeer())
+
+		case *pb.QuaiProtocolMessage:
+			// TODO: handle quai protocol message
+		default:
+			log.Errorf("unknown message type received: %s", msg)
+			// TODO: handle unknown message type
+		}
 	}
-
-	// TODO: add logic to handle other requests
-
 }
