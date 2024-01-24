e5d08f216f7d1d9ffd2ffaeb33787a93df381e89
Alejo Acosta <alejoacos@gmail.com>
2024-01-24 14:33:17 -0300

    refactor read/write stream functions to use length prefix framing

--- common/stream_services.go
+++ common/stream_services.go
@@ -1,10 +1,10 @@
 package common
 
 import (
+	"encoding/binary"
 	"io"
 	"time"
 
-	"github.com/dominant-strategies/go-quai/log"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/pkg/errors"
 )
@@ -13,49 +13,52 @@ const (
 	// timeout in seconds before a read/write operation on the stream is considered failed
 	// TODO: consider making this dynamic based on the network latency
 	C_STREAM_TIMEOUT = 10 * time.Second
-
-	// buffer size in bytes (1MB)
-	C_BUFFER_SIZE = 1024 * 1024
 )
 
 // Reads the message from the stream and returns a byte of data.
 func ReadMessageFromStream(stream network.Stream) ([]byte, error) {
 	// Set a read deadline
-	err := stream.SetReadDeadline(time.Now().Add(C_STREAM_TIMEOUT))
-	if err != nil {
+	if err := stream.SetReadDeadline(time.Now().Add(C_STREAM_TIMEOUT)); err != nil {
 		return nil, errors.Wrap(err, "failed to set read deadline")
 	}
 
-	buffer := make([]byte, C_BUFFER_SIZE)
-	var data []byte
-	for {
-		n, err := stream.Read(buffer)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			log.Errorf("error reading message from stream: %s", err)
-			return nil, err
-		}
-		data = append(data, buffer[:n]...)
+	// First read the length of the incoming message
+	lenBytes := make([]byte, 4)
+	if _, err := io.ReadFull(stream, lenBytes); err != nil {
+		return nil, errors.Wrap(err, "failed to read message length")
 	}
+	msgLen := binary.BigEndian.Uint32(lenBytes)
 
-	log.Tracef("succesfully read %d bytes from stream", len(data))
+	// Now read the message
+	data := make([]byte, msgLen)
+	if _, err := io.ReadFull(stream, data); err != nil {
+		return nil, errors.Wrap(err, "failed to read message")
+	}
 
 	return data, nil
 }
 
 // Writes the message to the stream.
 func WriteMessageToStream(stream network.Stream, msg []byte) error {
-	err := stream.SetWriteDeadline(time.Now().Add(C_STREAM_TIMEOUT))
-	if err != nil {
+	// Set the write deadline
+	if err := stream.SetWriteDeadline(time.Now().Add(C_STREAM_TIMEOUT)); err != nil {
 		return errors.Wrap(err, "failed to set write deadline")
 	}
 
-	b, err := stream.Write(msg)
+	// Get the length of the message and convert it into 4 bytes
+	msgLen := uint32(len(msg))
+	lenBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(lenBytes, msgLen)
+
+	// First write the length of the message
+	if _, err := stream.Write(lenBytes); err != nil {
+		return errors.Wrap(err, "failed to write message length to stream")
+	}
+
+	// Then write the message itself
+	_, err := stream.Write(msg)
 	if err != nil {
 		return errors.Wrap(err, "failed to write message to stream")
 	}
-	log.Tracef("succesfully wrote %d bytes to stream", b)
-	return err
+	return nil
 }
