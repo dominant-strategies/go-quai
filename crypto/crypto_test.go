65a826eef41b991a7c19a608cc44a3bc86f6bec5
Anton Micallef <45823586+antomica29@users.noreply.github.com>
2024-02-15 19:01:47 +0100

    Refactor crypto_test.go

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -30,21 +30,24 @@ import (
 	"github.com/dominant-strategies/go-quai/common/hexutil"
 )
 
-var testAddrHex = "970e8128ab834e8eac17ab8e3812f010678cf791"
-var testPrivHex = "289c2857d4598e37fb9647507e47a309d6133539bf21a8b9cb6df88fd5232032"
+var (
+	testAddrHex        = "970e8128ab834e8eac17ab8e3812f010678cf791"
+	testPrivHex        = "289c2857d4598e37fb9647507e47a309d6133539bf21a8b9cb6df88fd5232032"
+	expectedKeccakHash = "4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45"
+)
 
 // These tests are sanity checks.
 // They should ensure that we don't e.g. use Sha3-224 instead of Sha3-256
 // and that the sha3 library uses keccak-f permutation.
 func TestKeccak256Hash(t *testing.T) {
 	msg := []byte("abc")
-	exp, _ := hex.DecodeString("4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45")
+	exp, _ := hex.DecodeString(expectedKeccakHash)
 	checkhash(t, "Sha3-256-array", func(in []byte) []byte { h := Keccak256Hash(in); return h[:] }, msg, exp)
 }
 
 func TestKeccak256Hasher(t *testing.T) {
 	msg := []byte("abc")
-	exp, _ := hex.DecodeString("4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45")
+	exp, _ := hex.DecodeString(expectedKeccakHash)
 	hasher := NewKeccakState()
 	checkhash(t, "Sha3-256-array", func(in []byte) []byte { h := HashData(hasher, in); return h[:] }, msg, exp)
 }
@@ -55,13 +58,6 @@ func TestToECDSAErrors(t *testing.T) {
 	}
 	if _, err := HexToECDSA("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); err == nil {
 		t.Fatal("HexToECDSA should've returned error")
-	}
-}
-
-func BenchmarkSha3(b *testing.B) {
-	a := []byte("hello world")
-	for i := 0; i < b.N; i++ {
-		Keccak256(a)
 	}
 }
 
@@ -287,7 +283,7 @@ func checkAddr(t *testing.T, addr0, addr1 common.Address) {
 // test to help Python team with integration of libsecp256k1
 // skip but keep it after they are done
 func TestPythonIntegration(t *testing.T) {
-	kh := "289c2857d4598e37fb9647507e47a309d6133539bf21a8b9cb6df88fd5232032"
+	kh := testPrivHex
 	k0, _ := HexToECDSA(kh)
 
 	msg0 := Keccak256([]byte("foo"))
