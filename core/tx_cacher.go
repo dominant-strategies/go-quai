6bde13419c3f160f7587e98134791f0ce673bc1c
Jonathan Downing <jonathan@dominantstrategies.io>
2024-02-26 17:28:24 -0600

    Only cache sender if tx has ECDSA signature

--- core/tx_cacher.go
+++ core/tx_cacher.go
@@ -62,7 +62,9 @@ func newTxSenderCacher(threads int) *txSenderCacher {
 func (cacher *txSenderCacher) cache() {
 	for task := range cacher.tasks {
 		for i := 0; i < len(task.txs); i += task.inc {
-			types.Sender(task.signer, task.txs[i])
+			if task.txs[i].Type() != types.QiTxType {
+				types.Sender(task.signer, task.txs[i])
+			}
 		}
 	}
 }
