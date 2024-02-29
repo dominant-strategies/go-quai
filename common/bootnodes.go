ac86473891d5e47f36c119c5b77ad043c4412685
Hussam <hussam@dominantstrategies.io>
2024-02-29 11:30:08 -0600

    Custom bootnodes

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -2,8 +2,7 @@ package common
 
 var (
 	BootstrapPeers = []string{
-		"/ip4/34.31.180.127/tcp/4001/p2p/12D3KooWJ8d3AnGzscdVf2kzxzTbJgDtDXqrHS8vnedrFmZr8uvK",
-		"/ip4/34.68.124.139/tcp/4001/p2p/12D3KooWLxXHZx2QGjJra2oWjGYQ3Vdrkew5cNZcEgfaiKi7Dfrx",
-		"/ip4/34.41.131.24/tcp/4001/p2p/12D3KooWD8kycTRgZAovnciaoyNzcJxW16hCFNSfJYm4Lj6ouCBd",
+		"/ip4/34.31.180.127/tcp/4001/p2p/12D3KooWMukBUrnZ2LqBTCg16qVjU642F2QLYj2KjhTq3dCoJiMo",
+		"/ip4/34.68.124.139/tcp/4001/p2p/12D3KooWRJoK7ebWk33DQmF97sfQV5R1k6avNT4E8aB6QC6fJskG",
 	}
 )
