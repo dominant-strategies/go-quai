aff45471b6c53433683f52e4edd97dd6a4b1ef6b
Hussam <hussam@dominantstrategies.io>
2024-04-15 09:52:50 -0500

    Set epochs to approximately one week

--- consensus/progpow/algorithm.go
+++ consensus/progpow/algorithm.go
@@ -41,7 +41,7 @@ const (
 	datasetGrowthBytes = 1 << 23 // Dataset growth per epoch
 	cacheInitBytes     = 1 << 24 // Bytes in cache at genesis
 	cacheGrowthBytes   = 1 << 17 // Cache growth per epoch
-	epochLength        = 30000   // Blocks per epoch
+	epochLength        = 4320    // Blocks per epoch
 	mixBytes           = 128     // Width of mix
 	hashBytes          = 64      // Hash length in bytes
 	hashWords          = 16      // Number of 32 bit ints in a hash
