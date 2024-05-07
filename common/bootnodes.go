c90bb648cb503d37ef7f942dbe434e5377360097
gop <gameofpointers@proton.me>
2024-05-07 07:35:10 -0500

    Added a local storage bootnode to the list

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -5,5 +5,6 @@ var (
 		"/ip4/146.148.66.22/tcp/4001/p2p/12D3KooWRQrLVEeJtfyKoJDYWYjryBKR8qxkDooMMzyf2ZpLaZRR",
 		"/ip4/35.190.147.237/tcp/4001/p2p/12D3KooWSb49ccXFWPCsvi7rzCbqBUK2xfuRC2xbo6KnUZk3YaVg",
 		"/ip4/35.194.7.78/tcp/4001/p2p/12D3KooWR3xMB6sCpsowQcvtdMKmKbTaiDcDFAXuWABdZVPWaVuo",
+		"/ip4/34.136.140.151/tcp/4001/p2p/12D3KooWJnWmBukEbZtGPPJvT1r4tQ97CRSGmnjHewcrjNB8oRxU",
 	}
 )
