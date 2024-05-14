7621c14ce18ac51077f8a4458058fd40375bb5e3
gop <gameofpointers@proton.me>
2024-05-14 14:39:29 -0500

    Marking peers as lively on the WorkObject and ProvideTopic

--- quai/p2p_backend.go
+++ quai/p2p_backend.go
@@ -90,6 +90,8 @@ func (qbe *QuaiBackend) OnNewBroadcast(sourcePeer p2p.PeerID, data interface{}, 
 		// TODO: Determine if the block information was lively or stale and rate
 		// the peer accordingly
 		backend.WriteBlock(&block)
+		// If it was a good broadcast, mark the peer as lively
+		qbe.p2pBackend.MarkLivelyPeer(sourcePeer, nodeLocation)
 	case types.Header:
 	case types.Transactions:
 		backend := *qbe.GetBackend(nodeLocation)
@@ -110,10 +112,12 @@ func (qbe *QuaiBackend) OnNewBroadcast(sourcePeer p2p.PeerID, data interface{}, 
 			return false
 		}
 		backend.SendWorkShare(&woHeader)
+	case types.ProvideTopic:
+		// Nothing to do now, but we may want to add some logic here later
+		// If it was a good broadcast, mark the peer as lively
+		qbe.p2pBackend.MarkLivelyPeer(sourcePeer, nodeLocation)
 	}
 
-	// If it was a good broadcast, mark the peer as lively
-	qbe.p2pBackend.MarkLivelyPeer(sourcePeer, nodeLocation)
 	return true
 }
 
