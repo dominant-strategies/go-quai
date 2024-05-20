976b60e4bb56e3cdd2abf54a1fd50eecd3912991
drk <joe@dominantstrategies.io>
2024-05-19 21:28:02 -0500

    bugfix: adjusted subscriptions so everyone subsrivbes to headers

--- cmd/utils/hierarchical_coordinator.go
+++ cmd/utils/hierarchical_coordinator.go
@@ -133,10 +133,7 @@ func (hc *HierarchicalCoordinator) startNode(logPath string, quaiBackend quai.Co
 	stack, apiBackend := makeFullNode(hc.p2p, location, hc.slicesRunning, hc.currentExpansionNumber, genesisBlock, logger)
 	quaiBackend.SetApiBackend(&apiBackend, location)
 
-	// Only subscribe to block and transaction data if processing state
-	if !quaiBackend.ProcessingState(location) && location.Context() == common.ZONE_CTX {
-		hc.p2p.Subscribe(location, &types.WorkObjectHeaderView{})
-	}
+	hc.p2p.Subscribe(location, &types.WorkObjectHeaderView{})
 
 	if quaiBackend.ProcessingState(location) && location.Context() == common.ZONE_CTX {
 		// Subscribe to the new topics after setting the api backend
