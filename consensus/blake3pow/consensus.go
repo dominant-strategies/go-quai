9d1249fdbe58441b9dc9bdc16d2a782cbb7eda5a
gop <gameofpointers@proton.me>
2024-06-21 11:08:45 -0500

    bugfix: fix the issue reading the quai genallocs

--- consensus/blake3pow/consensus.go
+++ consensus/blake3pow/consensus.go
@@ -607,7 +607,7 @@ func (blake3pow *Blake3pow) Finalize(chain consensus.ChainHeaderReader, header *
 		}
 		state.CreateAccount(lockupContract)
 
-		alloc := core.ReadGenesisAlloc("genallocs/gen_quai_alloc_"+nodeLocation.Name()+".json", blake3pow.logger)
+		alloc := core.ReadGenesisAlloc("genallocs/gen_alloc_quai_"+nodeLocation.Name()+".json", blake3pow.logger)
 		blake3pow.logger.WithField("alloc", len(alloc)).Info("Allocating genesis accounts")
 
 		for addressString, account := range alloc {
