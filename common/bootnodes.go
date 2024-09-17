922a8d8b15079cc36010d73c46423cf8982a23e5
Hussam <hussam@dominantstrategies.io>
2024-09-17 16:04:44 -0500

    Fix Orchard bootnode keys

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -12,9 +12,9 @@ var (
 			"/dns4/bootnode.garden3.quai.network/tcp/4001/p2p/12D3KooWJnWmBukEbZtGPPJvT1r4tQ97CRSGmnjHewcrjNB8oRxU",
 		},
 		"orchard": {
-			"/dns4/bootnode.orchard0.quai.network/tcp/4001/p2p/12D3KooWSu4bS67yLVUcFRdbrUbjQJdNeqe1cJ4pjHf77zwX5AHy",
-			"/dns4/bootnode.orchard1.quai.network/tcp/4001/p2p/12D3KooWBdtyVjqXSG3zE7qEz6K1wu3Cjc9B6A9bsh9A7xRwg1nP",
-			"/dns4/bootnode.orchard2.quai.network/tcp/4001/p2p/12D3KooWRdAA7rCedzbRewzw2R8zAY1QDiiA1KhDRxHXsZvukeBC",
+			"/dns4/bootnode.orchard0.quai.network/tcp/4001/p2p/12D3KooWBv5C4tSS72nBdG6Q12s7vSHYHtFcquBxAKDkfrrzseUz",
+			"/dns4/bootnode.orchard1.quai.network/tcp/4001/p2p/12D3KooWBAkaxYwJUenjVQPyvtZx6XWjtosyVzBRJxM7wthbWRE5",
+			"/dns4/bootnode.orchard2.quai.network/tcp/4001/p2p/12D3KooWNN1TqsrEEmitkk1LefwLNgut621sSCdncPoyMVoYT1v4",
 		},
 		"lighthouse": {
 			"/dns4/host-go-quai/tcp/4001/p2p/12D3KooWS83uhvCfyNeAV24nEsp3DHrygDD39rZiVy6Gabv6pqxt",
