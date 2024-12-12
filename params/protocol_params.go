bf9d45ac4859a94c98a7f1f9a9630d18cf105958
gop <gameofpointers@proton.me>
2024-12-12 15:51:59 -0600

    Increased the maximum includable workshares in the block to 25

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -173,7 +173,7 @@ var (
 	DifficultyAdjustmentPeriod        = big.NewInt(720)                                             // This is the number of blocks over which the average has to be taken
 	DifficultyAdjustmentFactor int64  = 40                                                          // This is the factor that divides the log of the change in the difficulty
 	MinQuaiConversionAmount           = new(big.Int).Mul(big.NewInt(10000000000), big.NewInt(GWei)) // 0.000000001 Quai
-	MaxWorkShareCount                 = 16
+	MaxWorkShareCount                 = 25
 	WorkSharesThresholdDiff           = 4 // Number of bits lower than the target that the default consensus engine uses
 	WorkSharesInclusionDepth          = 3 // Number of blocks upto which the work shares can be referenced and this is protocol enforced
 	LockupByteToBlockDepth            = make(map[uint8]uint64)
