daab8a156ed0f82293af80bebaec4dfa60d5a476
gop <gameofpointers@proton.me>
2025-01-28 17:56:40 -0600

    Updated the time to start tx to 6 days to line up with Feb10 transaction start

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -168,7 +168,7 @@ var (
 	OrchardDurationLimit              = big.NewInt(5) // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 	LighthouseDurationLimit           = big.NewInt(5) // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 	LocalDurationLimit                = big.NewInt(1) // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
-	TimeToStartTx              uint64 = 5 * BlocksPerDay
+	TimeToStartTx              uint64 = 6 * BlocksPerDay
 	BlocksPerDay               uint64 = new(big.Int).Div(big.NewInt(86400), DurationLimit).Uint64() // BlocksPerDay is the number of blocks per day assuming 5 second block time
 	BlocksPerWeek              uint64 = 7 * BlocksPerDay
 	BlocksPerMonth             uint64 = 30 * BlocksPerDay
