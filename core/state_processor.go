6094017c3d7e60b289e5ec6a98a091e359199959
Jonathan Downing <jonathan@dominantstrategies.io>
2025-01-31 15:09:21 -0600

    Disable coinbase lockup precompile for 1 month

--- core/state_processor.go
+++ core/state_processor.go
@@ -587,7 +587,7 @@ func (p *StateProcessor) Process(block *types.WorkObject, batch ethdb.Batch) (ty
 						if err != nil {
 							return nil, nil, nil, nil, 0, 0, 0, nil, nil, fmt.Errorf("coinbase tx %x has invalid contract: %w", tx.Hash(), err)
 						}
-						if statedb.GetCode(internal) == nil {
+						if statedb.GetCode(internal) == nil || block.NumberU64(common.ZONE_CTX) < params.CoinbaseLockupPrecompileKickInHeight {
 							// No code at contract address
 							// Coinbase reward is lost
 							// Justification: We should not store a coinbase lockup that can never be claimed
@@ -645,7 +645,7 @@ func (p *StateProcessor) Process(block *types.WorkObject, batch ethdb.Batch) (ty
 						if err != nil {
 							return nil, nil, nil, nil, 0, 0, 0, nil, nil, fmt.Errorf("coinbase tx %x has invalid recipient: %w", tx.Hash(), err)
 						}
-						if statedb.GetCode(internal) == nil {
+						if statedb.GetCode(internal) == nil || block.NumberU64(common.ZONE_CTX) < params.CoinbaseLockupPrecompileKickInHeight {
 							// No code at contract address
 							// Coinbase reward is lost
 							// Justification: We should not store a coinbase lockup that can never be claimed
--- core/worker.go
+++ core/worker.go
@@ -1018,7 +1018,7 @@ func (w *worker) commitTransaction(env *environment, parent *types.WorkObject, t
 				if err != nil {
 					return nil, false, fmt.Errorf("coinbase tx %x has invalid recipient: %w", tx.Hash(), err)
 				}
-				if env.state.GetCode(internal) == nil {
+				if env.state.GetCode(internal) == nil || env.wo.NumberU64(common.ZONE_CTX) < params.CoinbaseLockupPrecompileKickInHeight {
 					// Coinbase data is either too long or too small
 					// Coinbase reward is lost
 					receipt := &types.Receipt{Type: tx.Type(), Status: types.ReceiptStatusFailed, GasUsed: gasUsedForCoinbase, TxHash: tx.Hash()}
@@ -1112,7 +1112,7 @@ func (w *worker) commitTransaction(env *environment, parent *types.WorkObject, t
 				return nil, false, fmt.Errorf("coinbase tx %x has invalid recipient: %w", tx.Hash(), err)
 			}
 
-			if env.state.GetCode(internal) == nil {
+			if env.state.GetCode(internal) == nil || env.wo.NumberU64(common.ZONE_CTX) < params.CoinbaseLockupPrecompileKickInHeight {
 				// Coinbase data is either too long or too small
 				// Coinbase reward is lost
 				receipt := &types.Receipt{Type: tx.Type(), Status: types.ReceiptStatusFailed, GasUsed: gasUsedForCoinbase, TxHash: tx.Hash()}
