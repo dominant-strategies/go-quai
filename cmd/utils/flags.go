8b1184fcb625a306b6d265c41a2410ad56b116ce
jdowning100 <jdowning@utexas.edu>
2025-12-29 00:54:22 -0600

    Add optional PROXY protocol to stratum for load balancer support

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -645,6 +645,7 @@ var StratumFlags = []Flag{
 	StratumVarDiffFlag,
 	StratumAPIAddrFlag,
 	StratumNameFlag,
+	StratumProxyProtocolFlag,
 }
 
 var (
@@ -682,6 +683,11 @@ var (
 		Name:  c_NodeFlagPrefix + "stratum-api-addr",
 		Value: "0.0.0.0:3336",
 		Usage: "listen address for stratum API endpoint",
+	}
+	StratumProxyProtocolFlag = Flag{
+		Name:  c_NodeFlagPrefix + "stratum-proxy-protocol",
+		Value: false,
+		Usage: "enable PROXY protocol support for stratum endpoints (for use behind load balancers like GCP Network LB)",
 	}
 )
 
