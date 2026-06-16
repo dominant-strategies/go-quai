af78af6ed57d0086035042ecc6d07a8b57e1f98e
gop <gameofpointers@proton.me>
2026-06-16 07:28:38 -0500

    Updated the bootnodes to now include dns4 instead of ip4

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -3,20 +3,15 @@ package common
 var (
 	BootstrapPeers = map[string][]string{
 		"colosseum": {
-			"/ip4/35.188.147.227/tcp/4002/p2p/12D3KooWB72Vs7gfLgvJsmR7cex7tG5WY7WDdqJD124Res3byDyk",
-			"/ip4/34.23.115.164/tcp/4002/p2p/12D3KooWPdEr7wZfcWg9d2EoX6cmnyx3GvEnV5pVJvRxMUbkKesB",
-			"/ip4/34.145.104.213/tcp/4002/p2p/12D3KooWMHTexdSq1zQ2Sn4L8zddEa8PWK8TGM56ZHszRuoUPoq1",
+			"/dns4/colosseum1.qu.ai/tcp/4002/p2p/12D3KooWB72Vs7gfLgvJsmR7cex7tG5WY7WDdqJD124Res3byDyk",
+			"/dns4/colosseum2.qu.ai/tcp/4002/p2p/12D3KooWPdEr7wZfcWg9d2EoX6cmnyx3GvEnV5pVJvRxMUbkKesB",
+			"/dns4/colosseum3.qu.ai/tcp/4002/p2p/12D3KooWMHTexdSq1zQ2Sn4L8zddEa8PWK8TGM56ZHszRuoUPoq1",
 		},
-		"garden": {
-			"/ip4/34.133.55.239/tcp/4002/p2p/12D3KooWPk7xbWnnsR73QZK8h2zabyp4iqZjyMHwrsBoRuWSXRfu",
-		},
+		"garden": {},
 		"orchard": {
-			"/ip4/35.196.106.212/tcp/4002/p2p/12D3KooWK755v2ynqqgvrEeTQe38E1i556jDwX9JcduotE1bfvjU",
-			"/ip4/34.136.242.207/tcp/4002/p2p/12D3KooWMjRbhyjQ5PCbt6qzMcVJx3n5NRx6uUv43yQP9ec3DV3U",
-			"/ip4/35.247.60.58/tcp/4002/p2p/12D3KooWS3GnpJMNqKSSP8eCGYw53xtwdRFVZ5jj1RsCnFX2R1eu",
+			"/dns4/orchard1.qu.ai/tcp/4002/p2p/12D3KooWK755v2ynqqgvrEeTQe38E1i556jDwX9JcduotE1bfvjU",
+			"/dns4/orchard2.qu.ai/tcp/4002/p2p/12D3KooWMjRbhyjQ5PCbt6qzMcVJx3n5NRx6uUv43yQP9ec3DV3U",
 		},
-		"lighthouse": {
-			"/ip4/34.27.85.224/tcp/4002/p2p/12D3KooWArCm6xBpMhJHTcM9BpWEv28ZeHY5vN8P4sDfJBdEYwVZ",
-		},
+		"lighthouse": {},
 	}
 )
